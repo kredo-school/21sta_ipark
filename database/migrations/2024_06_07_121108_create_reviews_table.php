1ea53fa8d75f366078140a74aae847a1ecbd3952
yyanagita <y.yuka346@gmail.com>
2024-06-07 22:16:35 +0900

    [migrations/users_table・reviews_table]
    Additions:reviews_table
    Changes:users_table

---
+++ database/migrations/2024_06_07_121108_create_reviews_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('parking_place_id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('comment');
+            $table->integer('star');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('parking_place_id')->references('id')->on('parking_places');
+            $table->foreign('user_id')->references('id')->on('users');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
