08fd1d68b0243424ba4247bdc50b6d50237588ea
YuriNakagawa0306 <yuchan.1drad5296.love@gmail.com>
2024-06-19 20:31:34 +0900

    [Backend/favorite_list]
    Changes:
    - Updated ProfileController.php, favorite.php and model of Favorite and User
      - made each button correspond to a transition to the next page or store
    - Updated LoginController.php, RegisterController.php and web.php and deleted welcome.php
      - we don't need welcome page so changed the routes, and also set up the middleware auth

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
@@ -66,9 +66,11 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
+            'username' => $data['username'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'phone'=> $data['phone'],
+            'car_type'=> $data['car_type'],
         ]);
     }
 }
