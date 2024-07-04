edd789c03c72632d923bfbd22322ca32ee2a75d6
yyanagita <y.yuka346@gmail.com>
2024-07-04 21:21:33 +0900

    [UI / ReservationHistory]
    Additions:
    add Reservation History page

--- routes/web.php
+++ routes/web.php
@@ -60,7 +60,6 @@ Route::group(["middleware"=>"auth"], function()
     Route::post('/favorite/store/{id}', [FavoriteController::class, 'store'])->name('favorite.store');
     Route::delete('/favorite/destroy/{id}', [FavoriteController::class, 'destroy'])->name('favorite.destroy');
 
-<<<<<<<<< Temporary merge branch 1
     //Reservations
     Route::get('/reservation/show/{id}', [ReservationsController::class, 'show'])->name('reservation.show');
     Route::post('/reservation/{id}', [ReservationsController::class, 'create'])->name('reservation.create');
