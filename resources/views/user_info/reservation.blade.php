50c3b7778c652d701f1d91b2ec4d40081f61e45f
yyanagita <y.yuka346@gmail.com>
2024-07-20 17:54:18 +0900

    Resister

--- resources/views/user_info/reservation.blade.php
+++ resources/views/user_info/reservation.blade.php
@@ -12,7 +12,9 @@
 <div class="container">
   <div class="row text-center">
       <div class="col-md-8 mb-4 page-title">
-          <div class="h1 text-start"><span class="underline">&nbsp;Res</span>ervation History</div>
+          <div class="page-title h1 text-start">
+            <span class="underline">&nbsp;Res</span>ervation History
+          </div>
       </div>
   </div>
   <div class="row ms-1 text-center">
@@ -43,94 +45,106 @@
   </div>
   
 
-  <div class="card mt-0 profile-card p-3 shadow mb-5">
+  <div class="card mt-0 profile-card py-3 shadow mb-5 custom-padding">
     <div class="card-body" style="color: #343A40; overflow-x: auto;">
       <div class="currentReservation">
         <h4 class="h1 mx-5 p-4 fw-bold">Current Reservation</h4>
         @if (empty($future_reservations))
           <h4 class="mx-5 p-4">No reservation yet</h4>
         @else
-          @foreach (array_reverse($future_reservations) as $future_reservation)
-            <div class="row border border-2 border-orange rounded-5 mx-5 my-4 p-4 shadow">
-              <div class="col-md-3">
-                <a class ="text-decoration-none" 
-                  href="{{route('showParkingDetail', $future_reservation->parking_place_id)}}">
-                  @if ($future_reservation->ParkingPlace->image != null)
-                      <img
-                        class="w-100"
-                        src="{{asset($future_reservation->ParkingPlace->image)}}"
-                        alt="{{$future_reservation->ParkingPlace->parking_place_name}}"
-                        style="height:200px"
-                      >
-                  @else
-                      <div class="no-image" style="height:200px">
-                          <p class="text-center h5">
-                              No image
-                          </p>
-                      </div>
-                  @endif
-                </a>  
-              </div>
-              <div class="col-md-3 border-end d-flex flex-column">
-                <div class="h4 fw-bold"
-                >
-                  {{$future_reservation->ParkingPlace->parking_place_name}}
+          <div class="reservation-list">
+            @foreach (array_reverse($future_reservations) as $future_reservation)
+              <div class="row border border-2 border-orange rounded-5 mx-5 my-4 p-4 shadow">
+                <div class="col-lg-3">
+                  <a 
+                    class ="text-decoration-none" 
+                    href="{{route('showParkingDetail', $future_reservation->parking_place_id)}}"
+                  >
+                    @if ($future_reservation->ParkingPlace->image != null)
+                        <img
+                          class="w-100"
+                          src="{{asset($future_reservation->ParkingPlace->image)}}"
+                          alt="{{$future_reservation->ParkingPlace->parking_place_name}}"
+                          style="height:200px"
+                        >
+                    @else
+                        <div class="no-image" style="height:200px">
+                            <p class="text-center h5">
+                                No image
+                            </p>
+                        </div>
+                    @endif
+                  </a>  
                 </div>
-                <h5 class="mt-2 mb-3">
-                    <i class="fa-solid fa-location-dot"></i>&nbsp;
-                    {{$future_reservation->ParkingPlace->city}}
-                </h5>
-                <div class="mt-auto ms-auto me-2">
-                    <a
-                        href="{{route('showParkingDetail', $future_reservation->parking_place_id)}}"
-                        class="btn btn-sm rounded-pill px-3 btn-orange fw-bold"
-                    >
-                        See detail
-                    </a>
-                </div>
-              </div>
-              <div class="col-md ms-1 mt-4 align-items-center">
-                <div class="row m-4 align-items-center">
-                  <div class="col-md">
-                    <div class="row">
-                      Start Time
-                    </div>
-                    <div class="row h5 text-center my-2 fw-bold">
-                      {{ \Carbon\Carbon::parse($future_reservation->planning_time_from)->format('H:i') }} <br>
-                      {{ $future_reservation->date }}
-                    </div>
-                  </div>
-                  <div class="col-md-2">
-                    <div class="row">
 
-                    </div>
-                    <div class="row">
-                      &nbsp;&nbsp;&nbsp;-->
-                    </div>
+                {{-- name, city, button --}}
+                <div class="col-lg-3 border-end d-flex flex-column">
+                  <a
+                    href="{{route('showParkingDetail', $future_reservation->parking_place_id)}}"
+                    class="h3 fw-bold mt-2 parking-place-name"
+                  >
+                    {{$future_reservation->ParkingPlace->parking_place_name}}
+                  </a>
+                  <h5 class="mt-2 mb-3 city-name">
+                      <i class="fa-solid fa-location-dot"></i>&nbsp;
+                      {{$future_reservation->ParkingPlace->city}}
+                  </h5>
+                  <div class="mt-auto ms-auto me-2">
+                      <a
+                          href="{{route('showParkingDetail', $future_reservation->parking_place_id)}}"
+                          class="btn btn-sm rounded-pill px-3 btn-orange fw-bold"
+                      >
+                          See detail
+                      </a>
                   </div>
-                  <div class="col-md">
-                    <div class="row">
-                      End Time
+                </div>
+
+                {{-- tinme --}}
+                <div class="col-md mt-4 align-items-center">
+                  <div class="row ms-4 mt-4 align-items-center">
+                    <div class="col-sm">
+                      <div class="row">
+                        Start Time
+                      </div>
+                      <div class="row h5 text-center my-2 fw-bold max-amount  res-time">
+                        {{ \Carbon\Carbon::parse($future_reservation->planning_time_from)->format('H:i') }} <br>
+                        {{ $future_reservation->date }}
+                      </div>
                     </div>
-                    <div class="row h5 text-center my-2 fw-bold">
-                      {{ \Carbon\Carbon::parse($future_reservation->planning_time_to)->format('H:i') }} <br>
-                      {{ $future_reservation->date }}
+                    <div class="col-sm-2">
+                      <div class="row">
+
+                      </div>
+                      <div class="row res-time">
+                        &nbsp;&nbsp;&nbsp;-->
+                      </div>
                     </div>
-                  </div>
-                  <div class="col-md">
-                    <a
-                      href="{{route('reservation.cancel', $future_reservation->id)}}"
-                      class="btn btn-sm rounded-pill px-3 btn-red-opposite fw-bold"
-                    >
-                        Cancel Reservation
-                    </a>
+                    <div class="col-sm">
+                      <div class="row">
+                        End Time
+                      </div>
+                      <div class="row h5 text-center my-2 fw-bold max-amount res-time">
+                        {{ \Carbon\Carbon::parse($future_reservation->planning_time_to)->format('H:i') }} <br>
+                        {{ $future_reservation->date }}
+                      </div>
+                    </div>
+                    <div class="col-md">
+                      <a
+                        href="{{route('reservation.cancel', $future_reservation->id)}}"
+                        class="btn btn-sm ad-btn rounded-pill px-3 btn-red-opposite fw-bold"
+                      >
+                          Cancel Reservation
+                      </a>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          @endforeach
+            @endforeach
+          </div>
         @endif
       </div>
+
+
       <div class="ReservationHistory">
         <h4 class="h1 mx-5 p-4 fw-bold">Reservation History</h4>
         @if (empty($past_reservations))
