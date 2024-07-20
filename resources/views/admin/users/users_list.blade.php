44c8ce33510a416d75fcbfb5822cdf62302277df
ayane504 <hkvp70_0531@yahoo.co.jp>
2024-07-20 15:44:01 +0900

    [ UI / responsive design ] Adjust following pages's UI to responsive design(small device size only) - admin user list - admin parking list
    -Fix some bugs in "register.blade.php"

--- resources/views/admin/users/users_list.blade.php
+++ resources/views/admin/users/users_list.blade.php
@@ -6,21 +6,21 @@
 
     <div class="container">
         <div class="row mt-4">
-            <div class="col-2 d-flex align-items-center">
-                <i class="fa-solid fa-car fa-2x"></i>
+            <div class="col-2 d-flex align-items-center admin-title-parking">
+                <i class="fa-solid fa-car fa-2x page-title"></i>
                 <span class="ms-2">
                     <a
                         href="{{ route('admin.parking.parkings_list') }}"
                         class="admin-parking-link"
                     >
-                        <span class="admin-parking h4"> Parking Places</span>
+                        <span class="admin-parking h4 page-title"> Parking Places</span>
                     </a>
                 </span>
             </div>
-            <div class="col-2 d-flex align-items-center justify-content-center">
-                <i class="fa-solid fa-user fa-2x admin-users-icon"></i>
+            <div class="col-2 d-flex align-items-center justify-content-center admin-title-users">
+                <i class="fa-solid fa-user fa-2x admin-users-icon page-title"></i>
                 <span class="ms-2">
-                    <span class="admin-users h4"> Users</span>
+                    <span class="admin-users h4 page-title"> Users</span>
                 </span>
             </div>
         </div>
@@ -28,94 +28,93 @@
         <form action="{{ route('admin.users_list')}}" method="get">
             <div class="card user-search mt-3">
                 <div class="row justify-content-center">
-                    <div class="col-10 mt-3">
-                        <div class="card-body fw-bold h5">
-                            <div class="row">
-                                <div class="col-md-5 ms-5">
-                                    <label for="inputUserName" class="form-label">User Name</label>
+                   
+                    <div class="card-body fw-bold h5 usersList-card-body">
+                        <div class="row">
+                            <div class="col-md-5 admin-search-group">
+                                <label for="inputUserName" class="form-label">User Name</label>
+                                <input
+                                    type="text"
+                                    class="form-control rounded-pill"
+                                    name="username"
+                                    value="{{ request('username') }}"
+                                >
+                            </div>
+                            <div class="col-md-6 ms-3 admin-search-group-r">
+                                <label class="form-label">Registered Date</label>
+                                <div class="input-group">
                                     <input
-                                        type="text"
+                                        type="date"
                                         class="form-control rounded-pill"
-                                        name="username"
-                                        value="{{ request('username') }}"
+                                        id="registeredDateFrom"
+                                        name="registeredDateFrom"
+                                        placeholder="From"
+                                        value="{{ request('registeredDateFrom') }}"
                                     >
-                                </div>
-                                <div class="col-md-6 ms-3">
-                                    <label class="form-label">Registered Date</label>
-                                    <div class="input-group">
-                                        <input
-                                            type="date"
-                                            class="form-control rounded-pill"
-                                            id="registeredDateFrom"
-                                            name="registeredDateFrom"
-                                            placeholder="From"
-                                            value="{{ request('registeredDateFrom') }}"
-                                        >
-                                        <span class="mx-2 pt-2">〜</span>
-                                        <input
-                                            type="date"
-                                            class="form-control rounded-pill"
-                                            id="registeredDateTo"
-                                            name="registeredDateTo"
-                                            placeholder="To"
-                                            value="{{ request('registeredDateTo') }}"
-                                        >
-                                    </div>
+                                    <span class="mx-2 pt-2">〜</span>
+                                    <input
+                                        type="date"
+                                        class="form-control rounded-pill"
+                                        id="registeredDateTo"
+                                        name="registeredDateTo"
+                                        placeholder="To"
+                                        value="{{ request('registeredDateTo') }}"
+                                    >
                                 </div>
                             </div>
+                        </div>
 
-                            <div class="row mt-4">
-                                <div class="col-md-5 ms-5">
-                                    <label for="inputEmail" class="form-label">E-mail</label>
-                                    <input
-                                        type="text"
-                                        class="form-control rounded-pill"
-                                        name="email" value="{{ request('email') }}"
-                                    >
-                                </div>
-                                <div class="col-md-6 ms-3 select-icon">
-                                    <label for="inputStatus" class="form-label">Status</label>
-                                    <select
-                                        name="status"
-                                        id="status"
-                                        class="form-control rounded-pill pic-icon custom-select-width"
-                                    >
-                                        <option value="" hidden>Please select type</option>
-                                        <option value="active" {{ request('status') == 'active' ? 'selected' : '' }}>
-                                            Active
-                                        </option>
-                                        <option value="inactive" {{ request('status') == 'inactive' ? 'selected' : '' }}>
-                                            Inactive
-                                        </option>
-                                    </select>
-                                </div>
+                        <div class="row mt-4">
+                            <div class="col-md-5 admin-search-group">
+                                <label for="inputEmail" class="form-label">E-mail</label>
+                                <input
+                                    type="text"
+                                    class="form-control rounded-pill"
+                                    name="email" value="{{ request('email') }}"
+                                >
                             </div>
+                            <div class="col-md-6 ms-3 select-icon admin-search-group-r">
+                                <label for="inputStatus" class="form-label">Status</label>
+                                <select
+                                    name="status"
+                                    id="status"
+                                    class="form-control rounded-pill pic-icon custom-select-width"
+                                >
+                                    <option value="" hidden>Please select type</option>
+                                    <option value="active" {{ request('status') == 'active' ? 'selected' : '' }}>
+                                        Active
+                                    </option>
+                                    <option value="inactive" {{ request('status') == 'inactive' ? 'selected' : '' }}>
+                                        Inactive
+                                    </option>
+                                </select>
+                            </div>
+                        </div>
 
-                            <div class="row my-4">
-                                <div class="col-md-5 ms-5">
-                                    <label for="inputPhone" class="form-label">Phone Number</label>
-                                    <input
-                                        type="number"
-                                        class="form-control rounded-pill"
-                                        name="phone"
-                                        value="{{ request('phone') }}"
+                        <div class="row my-4">
+                            <div class="col-md-5 admin-search-group">
+                                <label for="inputPhone" class="form-label">Phone Number</label>
+                                <input
+                                    type="number"
+                                    class="form-control rounded-pill"
+                                    name="phone"
+                                    value="{{ request('phone') }}"
+                                >
+                            </div>
+                            <div class="col-md-6 ms-3 admin-search-group-r">
+                                <div class="d-flex user-apply-btn">
+                                    <a
+                                        href="{{ route('admin.users_list') }}"
+                                        class="btn btn-red me-2 rounded-pill fw-bold"
                                     >
-                                </div>
-                                <div class="col-md-6 ms-3 apply-btn">
-                                    <div class="d-flex">
-                                        <a
-                                            href="{{ route('admin.users_list') }}"
-                                            class="btn btn-red me-2 rounded-pill fw-bold"
-                                        >
-                                            Clear All Filter
-                                        </a>
-                                        <button
-                                            type="submit"
-                                            class="btn btn-red-opposite rounded-pill apply-ft-btn fw-bold"
-                                        >
-                                            Apply Filter
-                                        </button>
-                                    </div>
+                                        Clear All Filter
+                                    </a>
+                                    <button
+                                        type="submit"
+                                        class="btn btn-red-opposite rounded-pill apply-ft-btn fw-bold"
+                                    >
+                                        Apply Filter
+                                    </button>
                                 </div>
                             </div>
                         </div>
@@ -164,79 +163,81 @@
             {{ $all_users->links('pagination::bootstrap-4') }}
         </div>
 
-        <table class="parking-list h6 table-hover align-center text-center w-100">
-            <thead>
-                <tr>
-                    <th>
-                        <i class="fa-solid fa-check"></i>
-                    </th>
-                    <th>User Name</th>
-                    <th>Email</th>
-                    <th>Phone Number</th>
-                    <th><i class="fa-solid fa-car fa-2x"></i></th>
-                    <th>Registered Date</th>
-                    <th>Status</th>
-                </tr>
-            </thead>
-            <tbody class="bg-white">
-                @forelse ($all_users as $user)
+        <div class="admin-table-responsive">
+            <table class="parking-list h6 table-hover align-center text-center w-100">
+                <thead>
                     <tr>
-                        @if ($user->deleted_at == null)
-                            <td>
-                                <input
-                                    form="user-list-form-delete"
-                                    type="checkbox"
-                                    id=""
-                                    name="user_ids[]"
-                                    value="{{ $user->id }}"
-                                >
-                            </td>
-                        @else
-                            <td>
-                                <input
-                                    form="user-list-form-restore"
-                                    type="checkbox"
-                                    id=""
-                                    name="user_ids[]"
-                                    value="{{ $user->id }}"
-                                >
-                            </td>
-                        @endif
-                        <td>
-                            <a
-                                href="{{ route('profile', $user->id)}}"
-                                class="text-decoration-none admin-username"
-                            >
-                                {{ $user->username }}
-                            </a>
-                        </td>
-                        <td>{{ $user->email }}</td>
-                        <td>{{ $user->phone }}</td>
-                        <td>
-                            @if($user->car_type)
-                                {{ $user->car_type }}
-                            @endif
-                        </td>
-                        <td>{{ $user->created_at }}</td>
-                        <td>
-                            @if($user->trashed())
-                                {{-- DEACTIVATE --}}
-                                <div class="deactivate-logo">
-                                    <i class="fa-solid fa-circle"></i>
-                                </div>
-                            @else
-                                {{-- ACTIVATE --}}
-                                <div class="activate-logo">
-                                    <i class="fa-solid fa-circle"></i>
-                                </div>
-                            @endif
-                        </td>
+                        <th>
+                            <i class="fa-solid fa-check"></i>
+                        </th>
+                        <th>User Name</th>
+                        <th>Email</th>
+                        <th>Phone Number</th>
+                        <th><i class="fa-solid fa-car fa-2x"></i></th>
+                        <th>Registered Date</th>
+                        <th>Status</th>
                     </tr>
-                @empty
-                    <tr class="text-center" aria-colspan="6">No users found.</tr>
-                @endforelse
-            </tbody>
-        </table>
+                </thead>
+                <tbody class="bg-white">
+                    @forelse ($all_users as $user)
+                        <tr>
+                            @if ($user->deleted_at == null)
+                                <td>
+                                    <input
+                                        form="user-list-form-delete"
+                                        type="checkbox"
+                                        id=""
+                                        name="user_ids[]"
+                                        value="{{ $user->id }}"
+                                    >
+                                </td>
+                            @else
+                                <td>
+                                    <input
+                                        form="user-list-form-restore"
+                                        type="checkbox"
+                                        id=""
+                                        name="user_ids[]"
+                                        value="{{ $user->id }}"
+                                    >
+                                </td>
+                            @endif
+                            <td>
+                                <a
+                                    href="{{ route('profile', $user->id)}}"
+                                    class="text-decoration-none admin-username"
+                                >
+                                    {{ $user->username }}
+                                </a>
+                            </td>
+                            <td>{{ $user->email }}</td>
+                            <td>{{ $user->phone }}</td>
+                            <td>
+                                @if($user->car_type)
+                                    {{ $user->car_type }}
+                                @endif
+                            </td>
+                            <td>{{ $user->created_at }}</td>
+                            <td>
+                                @if($user->trashed())
+                                    {{-- DEACTIVATE --}}
+                                    <div class="deactivate-logo">
+                                        <i class="fa-solid fa-circle"></i>
+                                    </div>
+                                @else
+                                    {{-- ACTIVATE --}}
+                                    <div class="activate-logo">
+                                        <i class="fa-solid fa-circle"></i>
+                                    </div>
+                                @endif
+                            </td>
+                        </tr>
+                    @empty
+                        <tr class="text-center" aria-colspan="6">No users found.</tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
         <div class="d-flex justify-content-center mt-5 userList-pagination">
             {{ $all_users->links('pagination::bootstrap-4') }}
         </div>
